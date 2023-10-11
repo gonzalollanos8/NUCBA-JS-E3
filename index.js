0b63001d5183fbe738da0ff53ebc871d0a04c0e0
Gonzalo Llanos <emanuelgonzalo98@gmail.com>
2023-10-11 01:35:13 -0300

    faltan estilos

--- index.js
+++ index.js
@@ -45,52 +45,80 @@ const pizzas = [
     imagen: "./img/anana.png",
   },
 ];
-//html
-let searchNumber = document.getElementById('searchNumber');
-let searchBtn = document.getElementById('searchBtn');
-let containerRender = document.getElementById('containerRender');
 
-//funciones 
+//elementos del html
+const selectPizza = document.getElementById('searchNumber');
+const searchPizza = document.getElementById('searchBtn');
+const renderContainer = document.getElementById('containerRender');
 
-//1 template
-const templateCard = ({id,nombre,precio,ingredientes,imagen}) =>{
-  return `<img src='${imagen}' alt='${nombre}'/>
-          <h2>${nombre}</h2>
-          <p>${ingredientes}</p>
-          <span>${precio}</span>
-          <p class='number-id'>${id}</p>
-          `
-}
-//2 render
-const renderCard = () => {
-  containerRender.innerHTML = pizzas.map((pizza)=> templateCard(pizza)).join('');
-};
 
-//render error
-const renderError = () => {containerRender.innerHTML = '<h2>No encontramos ninguna pizza con ese ID</h2>'};
+//validacion de formulario
+const error = (pizzaValid) =>{
+  isValid = true;
 
-//3 validacion de render
+  if(!pizzaValid.length){
+    renderContainer.innerHTML = `<h2>No seleccionaste ninguna pizza</h2>`
+    return;
+    }else if (pizzaValid > pizzas.length){
+    renderContainer.innerHTML = `<h2>La pizza que buscas, no se encuentra en la lista</h2>`
+    return
+    };
 
-const validId = () =>{
+    return isValid;
+  };
 
-  if(searchNumber == pizzas.id){
-    renderCard();
-  }else{
-    renderError()
+
+//guardo en local storage
+  const saveLocalstorage = (save) => {
+    localStorage.setItem('savePizza', JSON.stringify(save))
+  };
+
+
+//render de pizza
+  const renderCard = (selectPizza) => {
+    const { nombre, ingredientes, precio, imagen } = selectPizza
+
+    return renderContainer.innerHTML = `
+    <div class="card">
+      <img src='${imagen}' alt='${nombre}'>
+      <h2>${nombre}</h2>
+      <p>Ingredientes: ${ingredientes.join('-')}</p>
+      <span>Precio: $${precio}</span>
+    </div>
+    `
+  };
+
+  const selection = (pizzaArr,select) =>{
+    const pizzaSelection = pizzaArr.filter((pizza) => pizza.id == select);
+    return pizzaSelection[0];
   }
-}
+
+  const pizza = () =>{
+    const pizzaValue = selectPizza.value;
+    renderCard.innerHTML = '';
+
+    if(error(pizzaValue)){
+      error.innerHTML = '';
+      saveLocalstorage(pizzaValue);
+      
+      const filterPizza = selection(pizzas,pizzaValue);
+      renderCard(filterPizza);
+    };
+  };
+
+  const savePizza = () =>{
+    const pizzaDefault = JSON.parse(localStorage.getItem('savePizza'));
+    if(pizzaDefault) {
+      const filterPizza = selection (pizzas, pizzaDefault);
+      renderCard(filterPizza);
+    }
+  }
 
 
+  const init = () =>{
+    selectPizza.addEventListener('submit', (e) => e.preventDefault);
+    document.addEventListener('DOMContentLoaded',savePizza);
+    searchPizza.addEventListener('click',pizza)
+  }
 
-
-
-//funcion inicial
-function init(){
-  // let mostrarPizzas = () => console.log(pizzas);
-  // mostrarPizzas();
-  validId()
-  console.log(searchNumber)
-  // renderCard()
-}
-
-init()
+  init()
